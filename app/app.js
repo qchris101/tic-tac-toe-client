53f7028afddd0809e589fc5165560aa490f90868
qchris101 <qchris101@yahoo.com>
2021-10-09 17:54:43 -0400

    Created app file and add sign up event

--- app/app.js
+++ app/app.js
@@ -1,9 +1,9 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
-
+const authEvents = require('./events')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  $('#sign-up').on('submit', console.log('i was clicked'))
 })
