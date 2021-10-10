647ecd4b331812fce9b64dfd231ac923eae40041
qchris101 <qchris101@yahoo.com>
2021-10-09 20:29:31 -0400

    Created Curl Script for sign in to test production Api through Terminal

---
+++ curl-scripts/sign-in.sh
@@ -0,0 +1,15 @@
+EMAIL=kol@email.com PASSWORD=p sh curl-scripts/auth/sign-up.sh
+
+
+curl "https://tic-tac-toe-api-production.herokuapp.com/sign-up" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
