3f3714d54a81206f8a4d5089d5e109739564237c
qchris101 <qchris101@yahoo.com>
2021-10-09 20:25:12 -0400

    Created functions that handle my submit events for Sign-In and Sign-Out

--- app/events.js
+++ app/events.js
@@ -2,7 +2,7 @@ const getFormFields = require('../lib/get-form-fields')
 const api = require('./api')
 const ui = require('./ui')
 
-const onSignUp = (event) => {
+const onSignUp = function (event) {
   event.preventDefault()
   const form = event.target
   const formData = getFormFields(form)
@@ -12,6 +12,26 @@ const onSignUp = (event) => {
     .catch(ui.signUpFailure)
 }
 
+const onSignIn = (event) => {
+  event.preventDefault()
+  const form = event.target
+  const formData = getFormFields(form)
+
+  api.signIn(formData)
+    .then(ui.signInSuccess)
+    .catch(ui.signInFailure)
+}
+const onSignOut = (event) => {
+  event.preventDefault()
+  const form = event.target
+  const formData = getFormFields(form)
+
+  api.signOut(formData)
+    .then(ui.onSignOutSuccess)
+    .catch(ui.onSignOutFailure)
+}
 module.exports = {
-  onSignUp
+  onSignUp,
+  onSignIn,
+  onSignOut
 }
