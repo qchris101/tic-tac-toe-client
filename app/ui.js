da98428be1158266939adde9cde1ba05b47abd42
qchris101 <qchris101@yahoo.com>
2021-10-09 20:26:56 -0400

    Created Success and Failure Messages (Currently not working properly)

--- app/ui.js
+++ app/ui.js
@@ -1,20 +1,63 @@
 'use strict'
-// const store = require('../store')
+const store = require('./store')
 
-const signUpSuccess = (responseData) => {
+const signUpSuccess = function (responseData) {
+  $('#movies-display').text('Signed up successfully!')
+  $('#movies-display').addClass('text-success')
+  $('form').trigger('reset')
 
+  console.log('responseData is', responseData)
 }
 
-const signUpFailure = (error) => {
+const signUpFailure = function (error) {
   $('#error-message').text('Sign up failed')
+  $('#error-message').removeClass()
+  $('#error-message').addClass('text-danger')
+
+  console.error('error is' + error)
+}
+
+const signInSuccess = (responseData) => {
+  store.user = responseData.user
+  $('#movies-display').text('Signed In successfully')
+  $('#movies-display').removeClass()
+  $('#movies-display').addClass('text-success')
+  $('form').trigger('reset')
+  $('#after-sign-in').show()
+  $('#before-sign-in').hide()
+}
+
+const signInFailure = (error) => {
+  $('#error-message').text('Sign In failed')
 
   $('#error-message').removeClass()
 
   $('#error-message').addClass('text-danger')
   console.error('error is' + error)
 }
+const signOutSuccess = (responseData) => {
+  store.user = responseData.user
+  $('#movies-display').text('Signed Out successfully')
+  $('#movies-display').removeClass()
+  $('#movies-display').addClass('text-success')
+  $('form').trigger('reset')
+  $('#after-sign-in').hide()
+  $('#before-sign-in').show()
+}
 
+const signOutFailure = (error) => {
+  $('#error-message').text('Sign In failed')
+
+  $('#error-message').removeClass()
+
+  $('#error-message').addClass('text-danger')
+  console.error('error is' + error)
+}
 module.exports = {
   signUpSuccess,
-  signUpFailure
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  signOutSuccess,
+  signOutFailure
 }
