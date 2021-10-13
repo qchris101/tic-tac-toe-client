c0db43943b180060fa6f3a6c3ad4077d3416b9cf
qchris101 <qchris101@yahoo.com>
2021-10-12 23:32:21 -0400

    Created Functions that respond to clicks and Store the data in an Array, along side current player function

--- app/events.js
+++ app/events.js
@@ -30,8 +30,124 @@ const onSignOut = (event) => {
     .then(ui.onSignOutSuccess)
     .catch(ui.onSignOutFailure)
 }
+
+let currentPlayer = 'X'
+const playerRotation = function () {
+  if (currentPlayer === '' || currentPlayer === 'X') {
+    currentPlayer = 'O'
+  } else if (currentPlayer === 'O') {
+    currentPlayer = 'X'
+  }
+  return currentPlayer
+}
+// const playerTurn = function () {
+//   $('#playerStatus').innerHTML = `It is currently ${currentPlayer} turn!`
+// }
+const spacesOnboard = ['', '', '', '', '', '', '', '', '']
+playerRotation()
+// playerTurn()
+const indexZero = function (event) {
+  event.preventDefault()
+  if (spacesOnboard[0] === '') {
+    spacesOnboard.splice(0, 1, playerRotation())
+    document.getElementById('indexZero').innerHTML = spacesOnboard[0]
+    console.log(spacesOnboard)
+  } else {
+    console.log('Whoops try somewhere else')
+  }
+}
+const indexOne = function (event) {
+  event.preventDefault()
+  if (spacesOnboard[1] === '') {
+    spacesOnboard.splice(1, 1, playerRotation())
+    document.getElementById('indexOne').innerHTML = spacesOnboard[1]
+    console.log(spacesOnboard)
+  } else {
+    console.log('This space is already used up!')
+  }
+}
+const indexTwo = function (event) {
+  event.preventDefault()
+  if (spacesOnboard[2] === '') {
+    spacesOnboard.splice(2, 1, playerRotation())
+    document.getElementById('indexTwo').innerHTML = spacesOnboard[2]
+  } else {
+    console.log('This space is already used up!')
+  }
+}
+const indexThree = function (event) {
+  event.preventDefault()
+  if (spacesOnboard[3] === '') {
+    spacesOnboard.splice(3, 1, playerRotation())
+    document.getElementById('indexThree').innerHTML = spacesOnboard[3]
+    console.log(spacesOnboard)
+  } else {
+    console.log('This space is already used up!')
+  }
+}
+const indexFour = function (event) {
+  event.preventDefault()
+  if (spacesOnboard[4] === '') {
+    spacesOnboard.splice(4, 1, playerRotation())
+    document.getElementById('indexFour').innerHTML = spacesOnboard[4]
+    console.log(spacesOnboard)
+  } else {
+    console.log('This space is already used up!')
+  }
+}
+const indexFive = function (event) {
+  event.preventDefault()
+  if (spacesOnboard[5] === '') {
+    spacesOnboard.splice(5, 1, playerRotation())
+    document.getElementById('indexFive').innerHTML = spacesOnboard[5]
+    console.log(spacesOnboard)
+  } else {
+    console.log('This space is already used up!')
+  }
+}
+const indexSix = function (event) {
+  event.preventDefault()
+  if (spacesOnboard[6] === '') {
+    spacesOnboard.splice(6, 1, playerRotation())
+    document.getElementById('indexSix').innerHTML = spacesOnboard[6]
+    console.log(spacesOnboard)
+  } else {
+    console.log('This space is already used up!')
+  }
+}
+const indexSeven = function (event) {
+  event.preventDefault()
+  if (spacesOnboard[7] === '') {
+    spacesOnboard.splice(7, 1, playerRotation())
+    document.getElementById('indexSeven').innerHTML = spacesOnboard[7]
+    console.log(spacesOnboard)
+  } else {
+    console.log('This space is already used up!')
+  }
+}
+const indexEight = function (event) {
+  event.preventDefault()
+  if (spacesOnboard[8] === '') {
+    spacesOnboard.splice(8, 1, playerRotation())
+    document.getElementById('indexEight').innerHTML = spacesOnboard[8]
+    console.log(spacesOnboard)
+  } else {
+    console.log('This space is already used up!')
+  }
+}
+
 module.exports = {
   onSignUp,
   onSignIn,
-  onSignOut
+  onSignOut,
+  indexZero,
+  indexOne,
+  indexTwo,
+  indexThree,
+  indexFour,
+  indexFive,
+  indexSix,
+  indexSeven,
+  indexEight
+
 }
